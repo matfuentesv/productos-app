b9e857ec05a3081a5fed6ce6856c0b92ffa7e107
matfuentesv <mat.fuentes@duocuc.cl>
2024-12-01 20:20:53 -0300

    avance codigo covertura

--- src/app/modules/account/account.component..spec.ts
+++ src/app/modules/account/account.component..spec.ts
@@ -1,7 +1,7 @@
-import { TestBed, ComponentFixture } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { AccountComponent } from './account.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {AccountComponent} from './account.component';
 
 describe('AccountComponent', () => {
   let component: AccountComponent;
@@ -40,5 +40,51 @@ describe('AccountComponent', () => {
     // Verifica que el formulario sea válido
     expect(component.accountForm.valid).toBeTrue();
   });
+
+  it('should mark the form as invalid if email is incorrect', () => {
+    component.accountForm.setValue({
+      firstName: 'Jane',
+      lastName: 'Doe',
+      rut: '19.033.397-3',
+      email: 'invalid-email',
+      phone: '987654321',
+      address: '123 Example Street',
+      password: 'StrongPass1',
+    });
+    expect(component.accountForm.valid).toBeFalse();
+    expect(component.accountForm.controls['email'].errors).toBeTruthy();
+  });
+
+  it('should mark the form as invalid if RUT is incorrect', () => {
+    component.accountForm.setValue({
+      firstName: 'Jane',
+      lastName: 'Doe',
+      rut: 'invalid-rut',
+      email: 'jane.doe@example.com',
+      phone: '987654321',
+      address: '123 Example Street',
+      password: 'StrongPass1',
+    });
+    expect(component.accountForm.valid).toBeFalse();
+    expect(component.accountForm.controls['rut'].errors).toBeTruthy();
+  });
+
+  it('should not submit the form if invalid', () => {
+    spyOn(console, 'log');
+    component.accountForm.setValue({
+      firstName: 'Jane',
+      lastName: '',
+      rut: '19.033.397-3',
+      email: 'jane.doe@example.com',
+      phone: '987654321',
+      address: '123 Example Street',
+      password: 'StrongPass1',
+    });
+    component.onSubmit();
+    expect(component.accountForm.valid).toBeFalse();
+    expect(console.log).not.toHaveBeenCalledWith('Form Submitted');
+  });
+
+
 
 });
