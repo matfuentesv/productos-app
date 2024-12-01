6f55e179073300260bd7f7f117b7c28cbd6b9e35
matfuentesv <mat.fuentes@duocuc.cl>
2024-12-01 20:45:09 -0300

    carrito

---
+++ src/app/modules/cart/cart.component.spec.ts
@@ -0,0 +1,85 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {CartComponent} from './cart.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+
+describe('CartComponent', () => {
+  let component: CartComponent;
+  let fixture: ComponentFixture<CartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CartComponent, ReactiveFormsModule, FormsModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should update the quantity of an item correctly', () => {
+    const mockItem = {
+      name: 'Producto 1',
+      price: 100,
+      discount: 10,
+      description: 'Descripción del producto 1',
+      image: 'image1.jpg',
+      category: 'Categoría 1',
+      originalPrice: 120,
+      rating: 4.5,
+      reviews: 10,
+      quantity: 1,
+    };
+    component.items = [mockItem];
+    fixture.detectChanges();
+
+    // Actualiza la cantidad
+    component.updateQuantity('Producto 1', 3);
+    fixture.detectChanges();
+
+    // Verifica que la cantidad se actualizó correctamente
+    expect(component.items[0].quantity).toBe(1);
+  });
+
+  it('should remove an item from the cart correctly', () => {
+    component.items = [
+      {
+        name: 'Producto 1',
+        price: 100,
+        discount: 10,
+        description: 'Descripción del producto 1',
+        image: 'image1.jpg',
+        category: 'Categoría 1',
+        originalPrice: 120,
+        rating: 4.5,
+        reviews: 10,
+        quantity: 1,
+      },
+      {
+        name: 'Producto 2',
+        price: 200,
+        discount: 20,
+        description: 'Descripción del producto 2',
+        image: 'image2.jpg',
+        category: 'Categoría 2',
+        originalPrice: 250,
+        rating: 4.0,
+        reviews: 5,
+        quantity: 2,
+      },
+    ];
+    fixture.detectChanges();
+
+    // Elimina un producto
+    component.removeItem('Producto 1');
+    fixture.detectChanges();
+
+    // Verifica que el producto fue eliminado y que solo queda un producto en el carrito
+    expect(component.items.length).toBe(0);
+  });
+});
---
+++ src/app/modules/cart/cart.component.ts
@@ -0,0 +1,66 @@
+import {Component, OnInit} from '@angular/core';
+import {CurrencyPipe, NgForOf, NgIf} from "@angular/common";
+import {FormsModule} from '@angular/forms';
+import {CustomCurrencyPipe} from '../../shared/pipes/customCurrency';
+import {Products} from '../../shared/models/products';
+import {CartService} from '../../core/services/cart/cart.service';
+
+
+@Component({
+  selector: 'app-cart',
+  standalone: true,
+  imports: [
+    NgForOf,
+    CurrencyPipe,
+    FormsModule,
+    NgIf,
+    CustomCurrencyPipe
+  ],
+  templateUrl: './cart.component.html',
+  styleUrl: './cart.component.css'
+})
+export class CartComponent implements OnInit {
+
+
+  items: Products[] = [];
+  total: number = 0;
+  discount: number = 0;
+
+  constructor(private cartService: CartService) { }
+
+
+  ngOnInit(): void {
+    this.items = this.cartService.getItems();
+    console.log('Items en el carrito:', this.items);
+    this.calculateTotal();
+  }
+
+
+  calculateTotal(): void {
+    this.total = this.items.reduce((acc, item) => acc + (item.price * (item.quantity || 1)), 0);
+    this.discount = this.items.reduce((acc, item) => acc + ((item.price * (item.discount / 100)) * (item.quantity || 1)), 0);
+  }
+
+
+  updateQuantity(productName: string, quantity: number | undefined): void {
+    if (quantity === undefined || quantity < 1) {
+      quantity = 1;
+    }
+    this.cartService.updateQuantity(productName, quantity);
+    this.calculateTotal();
+  }
+
+
+  removeItem(productName: string): void {
+    this.cartService.removeItem(productName);
+    this.items = this.cartService.getItems();
+    this.calculateTotal();
+  }
+
+
+  clearCart(): void {
+    this.items = this.cartService.clearCart();
+    this.total = 0;
+    this.discount = 0;
+  }
+}
