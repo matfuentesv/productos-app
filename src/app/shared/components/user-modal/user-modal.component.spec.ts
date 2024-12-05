7999960ccc163c514439e7598724ac822ea160ca
matfuentesv <mat.fuentes@duocuc.cl>
2024-12-04 22:09:21 -0300

    avance pruebas user modal

--- src/app/shared/components/user-modal/user-modal.component.spec.ts
+++ src/app/shared/components/user-modal/user-modal.component.spec.ts
@@ -1,95 +1,146 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {ReactiveFormsModule} from '@angular/forms';
-import {MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
-import {MatSnackBar} from '@angular/material/snack-bar';
-import {DataService} from '../../../core/services/data/data.service';
+import {TestBed} from '@angular/core/testing';
 import {UserModalComponent} from './user-modal.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {FormBuilder} from '@angular/forms';
+import {of} from 'rxjs';
+import {DataService} from '../../../core/services/data/data.service';
+import {HttpClientModule} from '@angular/common/http';
+
+const mockUsers = [
+  {
+    id: 1,
+    firstName: 'Original Name',
+    lastName: 'Last Name',
+    rut: '19.033.397-3',
+    email: 'original@example.com',
+    phone: '123456789',
+    address: '123 Street',
+    password: 'Password1',
+    roles: ['customer'],
+  },
+];
 
 describe('UserModalComponent', () => {
   let component: UserModalComponent;
-  let fixture: ComponentFixture<UserModalComponent>;
-  let mockDialog: jasmine.SpyObj<MatDialog>;
-  let mockDialogRef: jasmine.SpyObj<MatDialogRef<UserModalComponent>>;
-  let mockDataService: jasmine.SpyObj<DataService>;
-  let mockSnackBar: jasmine.SpyObj<MatSnackBar>;
-
-  beforeEach(async () => {
-    mockDialog = jasmine.createSpyObj('MatDialog', ['closeAll']);
-    mockDialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
-    mockDataService = jasmine.createSpyObj('DataService', ['addUser']);
-    mockSnackBar = jasmine.createSpyObj('MatSnackBar', ['open']);
-
-    await TestBed.configureTestingModule({
-      imports: [
-        ReactiveFormsModule,
-        MatDialogModule,
-        BrowserAnimationsModule,
-        UserModalComponent
-      ],
-      providers: [
-        { provide: MatDialog, useValue: mockDialog },
-        { provide: MatDialogRef, useValue: mockDialogRef },
-        { provide: DataService, useValue: mockDataService },
-        { provide: MAT_DIALOG_DATA, useValue: { users: [] } },
-        { provide: MatSnackBar, useValue: mockSnackBar }
-      ]
-    }).compileComponents();
-  });
+  let snackBar: jasmine.SpyObj<MatSnackBar>;
+  let dialogRef: jasmine.SpyObj<MatDialogRef<UserModalComponent>>;
+  let dataService: jasmine.SpyObj<DataService>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UserModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    snackBar = jasmine.createSpyObj('MatSnackBar', ['open']);
+    dialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
+    dataService = jasmine.createSpyObj('DataService', ['addUser']);
+
+    TestBed.configureTestingModule({
+      imports: [UserModalComponent, HttpClientModule],
+      providers: [
+        { provide: MatSnackBar, useValue: snackBar },
+        { provide: MatDialogRef, useValue: dialogRef },
+        { provide: DataService, useValue: dataService },
+        { provide: MAT_DIALOG_DATA, useValue: { users: mockUsers } },
+        FormBuilder,
+      ],
+    });
+
+    component = TestBed.createComponent(UserModalComponent).componentInstance;
+    component.user = [...mockUsers];
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should add a new user and show a success snackbar', () => {
+    // Mock del servicio de agregar usuario
+    dataService.addUser.and.returnValue(of({}));
+
+    // Inicializar el formulario
+    component.ngOnInit();
+
+    // Configurar datos válidos en el formulario
+    component.userForm.setValue({
+      firstName: 'John',
+      lastName: 'Doe',
+      rut: '19.033.397-3',
+      email: 'john@example.com',
+      phone: '123456789',
+      address: '123 Street',
+      password: 'Password1',
+      roles: 'admin',
+    });
+
+    // Llamar a onSubmit
+    component.onSubmit();
+
+    // Verificar las expectativas
+    expect(dataService.addUser).toHaveBeenCalledWith([
+      {
+        id: 1,
+        firstName: 'Original Name',
+        lastName: 'Last Name',
+        rut: '19.033.397-3',
+        email: 'original@example.com',
+        phone: '123456789',
+        address: '123 Street',
+        password: 'Password1',
+        roles: ['customer'],
+      },
+      {
+        id: jasmine.any(Number),
+        firstName: 'John',
+        lastName: 'Doe',
+        rut: '19.033.397-3',
+        email: 'john@example.com',
+        phone: '123456789',
+        address: '123 Street',
+        password: 'Password1',
+        roles: ['admin'],
+      },
+    ]);
+    expect(snackBar.open).toHaveBeenCalledWith(
+      'Usuario creado correctamente!',
+      '',
+      { duration: 3000, horizontalPosition: 'start', verticalPosition: 'bottom', panelClass: ['custom-snackbar'] }
+    );
+    expect(dialogRef.close).toHaveBeenCalledWith(1);
   });
 
-  it('should initialize the form', () => {
-    expect(component.userForm).toBeDefined();
-    expect(component.userForm.controls['firstName']).toBeDefined();
-    expect(component.userForm.controls['lastName']).toBeDefined();
-    expect(component.userForm.controls['rut']).toBeDefined();
-    expect(component.userForm.controls['email']).toBeDefined();
-    expect(component.userForm.controls['phone']).toBeDefined();
-    expect(component.userForm.controls['address']).toBeDefined();
-    expect(component.userForm.controls['password']).toBeDefined();
-    expect(component.userForm.controls['roles']).toBeDefined();
-  });
-
-  it('should close the dialog with a value', () => {
-    component.close(1);
-    expect(mockDialogRef.close).toHaveBeenCalledWith(1);
-  });
-
-
-
-  it('should not add a new user if form is invalid', () => {
+  it('should not add user if form is invalid', () => {
+    // Inicializar el formulario con datos inválidos
+    component.ngOnInit();
     component.userForm.setValue({
       firstName: '',
-      lastName: '',
+      lastName: 'Smith',
       rut: '',
       email: '',
       phone: '',
       address: '',
       password: '',
-      roles: ''
+      roles: '',
     });
 
+    // Llamar a onSubmit
     component.onSubmit();
 
-    expect(mockDataService.addUser).not.toHaveBeenCalled();
-    expect(mockSnackBar.open).not.toHaveBeenCalled();
-    expect(mockDialogRef.close).not.toHaveBeenCalled();
+    // Verificar que no se llamaron los métodos
+    expect(dataService.addUser).not.toHaveBeenCalled();
+    expect(snackBar.open).not.toHaveBeenCalled();
+    expect(dialogRef.close).not.toHaveBeenCalled();
   });
 
-  it('should validate numbers correctly', () => {
-    const event = { charCode: 49 }; // '1'
-    expect(component.validateNumbers(event)).toBeTrue();
-
-    const invalidEvent = { charCode: 65 }; // 'A'
-    expect(component.validateNumbers(invalidEvent)).toBeFalse();
+  it('should return true for valid numeric input', () => {
+    const validEvent = { charCode: 50 }; // Char code for '2'
+    const result = component.validateNumbers(validEvent);
+    expect(result).toBeTrue();
   });
+
+  it('should return true for "+" character input', () => {
+    const plusEvent = { charCode: 107 }; // Char code for '+'
+    const result = component.validateNumbers(plusEvent);
+    expect(result).toBeTrue();
+  });
+
+  it('should return false for non-numeric input', () => {
+    const invalidEvent = { charCode: 65 }; // Char code for 'A'
+    const result = component.validateNumbers(invalidEvent);
+    expect(result).toBeFalse();
+  });
+
 });
