b1408dc6e750a5f1daf595c8c318f6adfb621115
matfuentesv <mat.fuentes@duocuc.cl>
2024-12-13 22:51:58 -0300

    fix archivos docker y endpoint

--- src/app/enviroments/endpoints.ts
+++ src/app/enviroments/endpoints.ts
@@ -1,38 +1,39 @@
 export const endpoints = {
-  findAllUsers: {
-    name: 'Users',
-    description: 'Obtiene Lista de usuarios',
-    path: 'http://localhost:8081/api/findAllUsers'
-  },
   findAllProduct: {
     name: 'Products',
     description: 'Obtiene lista de productos',
     path: 'http://localhost:8080/api/findAllProduct'
   },
+  findAllUsers: {
+    name: 'Users',
+    description: 'Obtiene Lista de usuarios',
+    path: 'http://localhost:8082/api/findAllUsers'
+  },
+
   login: {
     name: 'Login User',
     description: 'Realiza login',
-    path: 'http://localhost:8081/api/login'
+    path: 'http://localhost:8082/api/login'
   },
   findUserByEmail: {
     name: 'Find user by email',
     description: 'Obtiene usuario po email',
-    path: 'http://localhost:8081/api/findUser/'
+    path: 'http://localhost:8082/api/findUser/'
   },
   createUser: {
     name: 'Create User',
     description: 'Crea usuarios',
-    path: 'http://localhost:8081/api/createUser'
+    path: 'http://localhost:8082/api/createUser'
   },
   updateUser: {
     name: 'UpdateUser',
     description: 'Actualiza información del usuario',
-    path: 'http://localhost:8081/api/updateUser'
+    path: 'http://localhost:8082/api/updateUser'
   },
   deleteUser: {
     name: 'Delete User',
     description: 'elimina usuario',
-    path: 'http://localhost:8081/api/deleteUser/'
+    path: 'http://localhost:8082/api/deleteUser/'
   },
   findAllOrders: {
     name: 'Find Orders',
@@ -42,7 +43,7 @@ export const endpoints = {
   payOrder:{
     name: 'Pay Orders',
     description: 'Pagar ordenes de compra',
-    path: 'http://localhost:8082/api/orders'
+    path: 'http://localhost:8081/api/orders'
   }
 
 }
